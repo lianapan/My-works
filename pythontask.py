5ce9a26a30882e618e4333b21e977dab5f850e6d
lianapan <liana.panosyan17@gmail.com>
2022-09-13 22:02:38 +0400

    Python code

---
+++ pythontask.py
@@ -0,0 +1,25 @@
+# def my_function(my_list):
+# 	for i in my_list:
+# 		new_list=my_list.sort(reverse=True)
+# 		return my_list[0]
+
+# print("Max number in my list is",my_function([4,1,2,12,15,8,21,24,3]))
+
+# def my_function(my_list):
+# 	max_number = my_list[0]
+# 	for number in my_list:
+# 		if number > max_number:
+# 			max_number = number
+# 	return max_number
+
+# print("Max number in my list is",my_function([4,1,2,12,15,8,47,24,3]))
+
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return (n * factorial(n-1))
+# num = int(input())
+
+# res = factorial(num)
+# print("The factorial of", num, "is", res)
